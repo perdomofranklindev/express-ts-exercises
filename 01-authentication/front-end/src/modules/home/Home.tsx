b011d91ede939a4cbd52a5439690349e1cfcf724
perdomofranklindev <perdomofranklindev@gmail.com>
2025-06-02 21:55:57 -0400

    Add Home component to integrate dashboard features including WelcomeSection, StatsOverview, QuickActions, RecentActivity, and SessionInfo for improved user interface.

---
+++ 01-authentication/front-end/src/modules/home/Home.tsx
@@ -0,0 +1,39 @@
+import { Box, Grid } from "@mui/material";
+import { WelcomeSection } from "./components/WelcomeSection";
+import { StatsOverview } from "./components/StatsOverview";
+import { SessionInfo } from "./components/SessionInfo";
+import { QuickActions } from "./components/QuickActions";
+import { RecentActivity } from "./components/RecentActivity";
+
+export const Home = () => {
+  return (
+    <Box>
+      <WelcomeSection />
+      <StatsOverview />
+      <SessionInfo />
+
+      {/* Quick Actions and Recent Activity */}
+      <Grid container spacing={3}>
+        {/* Quick Actions */}
+        <Grid
+          size={{
+            xs: 12,
+            md: 6,
+          }}
+        >
+          <QuickActions />
+        </Grid>
+
+        {/* Recent Activity */}
+        <Grid
+          size={{
+            xs: 12,
+            md: 6,
+          }}
+        >
+          <RecentActivity />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
