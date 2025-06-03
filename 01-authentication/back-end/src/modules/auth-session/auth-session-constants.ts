184a1d64eb0ad94b16f1f0fbf5c5dd722a1cf76e
perdomofranklindev <perdomofranklindev@gmail.com>
2025-06-02 21:55:03 -0400

    Update authentication session constants to extend access token expiration from 1 minute to 1 hour, improving session longevity and user experience.

--- 01-authentication/back-end/src/modules/auth-session/auth-session-constants.ts
+++ 01-authentication/back-end/src/modules/auth-session/auth-session-constants.ts
@@ -5,10 +5,10 @@ const oneMinute = oneSecond * 60;
 const oneHour = oneMinute * 60;
 const oneDay = oneHour * 24;
 
-const COOKIE_ACCESS_TOKEN_MAX_AGE = oneMinute;
+const COOKIE_ACCESS_TOKEN_MAX_AGE = oneHour;
 const COOKIE_REFRESH_TOKEN_MAX_AGE = oneDay * 7;
 
-const ACCESS_TOKEN_EXPIRES_IN: ms.StringValue = '1m';
+const ACCESS_TOKEN_EXPIRES_IN: ms.StringValue = '1h';
 const REFRESH_TOKEN_EXPIRES_IN: ms.StringValue = '7d';
 
 // You're gonna need both, in string and number.
