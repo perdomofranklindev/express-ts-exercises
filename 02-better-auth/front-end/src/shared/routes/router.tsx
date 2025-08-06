a7da2002d856b7d7b4fe4e38ea9fe3368c5065c5
perdomofranklindev <perdomofranklindev@gmail.com>
2025-08-05 22:02:19 -0400

    Refactor router structure to enhance readability and maintainability

--- 02-better-auth/front-end/src/shared/routes/router.tsx
+++ 02-better-auth/front-end/src/shared/routes/router.tsx
@@ -3,36 +3,54 @@ import SignInPage from "../../modules/auth/pages/SignInPage";
 import SignUpPage from "../../modules/auth/pages/SignUpPage";
 import UserAccountPage from "../../modules/user/pages/UserAccountPage";
 import { createBrowserRouter, Outlet } from "react-router-dom";
+import { AuthProvider, ProtectedRoute, PublicRoute } from "../auth";
 
 export const router = createBrowserRouter([
   {
-    path: "/auth",
-    element: <Outlet />,
+    element: (
+      <AuthProvider>
+        <Outlet />
+      </AuthProvider>
+    ),
     children: [
       {
-        path: "sign-in",
-        element: <SignInPage />,
-      },
-      {
-        path: "sign-up",
-        element: <SignUpPage />,
-      },
-    ],
-  },
-  {
-    path: "/",
-    element: <Outlet />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: "user",
+        path: "/auth",
+        element: (
+          <PublicRoute>
+            <Outlet />
+          </PublicRoute>
+        ),
         children: [
           {
-            path: "account",
-            element: <UserAccountPage />,
+            path: "sign-in",
+            element: <SignInPage />,
+          },
+          {
+            path: "sign-up",
+            element: <SignUpPage />,
+          },
+        ],
+      },
+      {
+        path: "/",
+        element: (
+          <ProtectedRoute>
+            <Outlet />
+          </ProtectedRoute>
+        ),
+        children: [
+          {
+            index: true,
+            element: <HomePage />,
+          },
+          {
+            path: "user",
+            children: [
+              {
+                path: "account",
+                element: <UserAccountPage />,
+              },
+            ],
           },
         ],
       },
